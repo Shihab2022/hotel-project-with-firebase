f8653d8dc3ce6295017241c32e7b9e8fd0c58c0b
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-04-12 12:40:12 +0600

    some code for sign in

--- src/Componant/SingIn/SingIn.js
+++ src/Componant/SingIn/SingIn.js
@@ -1,9 +1,20 @@
 import React from 'react';
-
+import './SingIn.css'
 const SingIn = () => {
     return (
-        <div>
-           <h2>from sign in</h2> 
+        <div className="form-container">
+            <h1 className="form-title">Sign In</h1>
+         <form >
+
+<div>
+<label htmlFor="Enter your email">Enter your email</label> <br />
+<input type="email"  required /></div>
+<label htmlFor="Enter your password">Enter your password</label> <br />
+<input type="password"  required />
+
+
+
+         </form>
         </div>
     );
 };
