0d34917e3dfd40d58c2619344acb7bc947a13558
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-04-13 22:01:00 +0600

    set up firebase init js

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,12 @@ import {getAuth} from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBPOEXmXfgwaxrkcQEA7vcY_5BFCwCn8po",
-  authDomain: "hotel-project-with-firebase.firebaseapp.com",
-  projectId: "hotel-project-with-firebase",
-  storageBucket: "hotel-project-with-firebase.appspot.com",
-  messagingSenderId: "941086905623",
-  appId: "1:941086905623:web:5aad1492e5cbaee33b1352"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain:process.env.REACT_APP_authDomain,
+  projectId:process.env.REACT_APP_projectId,
+  storageBucket:process.env.REACT_APP_storageBucket,
+  messagingSenderId:process.env.REACT_APP_messagingSenderId,
+  appId:process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
